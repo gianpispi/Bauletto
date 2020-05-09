2c705cb0a61d7f5a7b76c47efbe406e16b5b072d
Francesco Bianco <f.bianco@aliaslab.net>
2020-05-09 11:59:57 +0200

    Fixed missing action assignment

--- Sources/Bauletto/Bauletto.swift
+++ Sources/Bauletto/Bauletto.swift
@@ -121,6 +121,7 @@ internal class BaulettoView: UIView {
         self.tintColor = settings?.tintColor ?? self.tintColor
         self.backgroundStyle = settings?.backgroundStyle ?? self.backgroundStyle
         self.dismissMode = settings?.dismissMode ?? self.dismissMode
+        self.action = action
     }
     
     public func animateIcon() {
